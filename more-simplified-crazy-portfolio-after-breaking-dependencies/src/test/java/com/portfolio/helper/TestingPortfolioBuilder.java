cdd450b03f7e0fe976937315019128b8bfee882e
freyes <fran@codesai.com>
2024-09-13 12:24:05 +0100

    add new exercises

---
+++ more-simplified-crazy-portfolio-after-breaking-dependencies/src/test/java/com/portfolio/helper/TestingPortfolioBuilder.java
@@ -0,0 +1,37 @@
+package com.portfolio.helper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestingPortfolioBuilder {
+    private final List<String> lines;
+    private String now;
+
+    TestingPortfolioBuilder()
+    {
+        lines = new ArrayList<>();
+        now = "";
+    }
+
+    public static TestingPortfolioBuilder aPortFolio()
+    {
+        return new TestingPortfolioBuilder();
+    }
+
+    public TestingPortfolioBuilder with(AssetsFileLinesBuilder lineBuilder)
+    {
+        lines.add(lineBuilder.build());
+        return this;
+    }
+
+    public TestingPortfolioBuilder onDate(String dateAsString)
+    {
+        now = dateAsString;
+        return this;
+    }
+
+    public TestingPortfolio build()
+    {
+        return new TestingPortfolio(lines, now);
+    }
+}
